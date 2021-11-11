17e613f1ef2942e4328a0e8ce543d813a600c1fa
smolesen <smolesen@hotmail.com>
2021-11-11 10:54:14 +0100

    C-Sharp publish demo project (#26)
    * C-Sharp publish demp project
    
    * Updated with suggestions from PullRequest.
    Added readmy.md
    
    * Removed .vs from .gitignore
    
    * Removed empty line
    
    Co-authored-by: Søren M. Olesen <solesen@grundfos.com>

---
+++ api/csharp/pubsub-publish/src/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Google.Protobuf;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace toit.demos.api.csharp.pubsubpublish
+{
+    public class Program
+    {
+        private static string apikey = "<apikey>"; // From: https://console.toit.io/project/apikeys
+        static async Task Main(string[] args)
+        {
+            var callCredentials = CallCredentials.FromInterceptor((context, metadata) =>
+            {
+                metadata.Add("Authorization", $"Bearer {apikey}");
+                return Task.CompletedTask;
+            });
+            var channelCredentials = ChannelCredentials.Create(new SslCredentials(), callCredentials);
+            using var channel = GrpcChannel.ForAddress("https://api.toit.io:443", new GrpcChannelOptions
+            {
+                Credentials = channelCredentials
+            });
+
+            var client = new Toit.Api.Pubsub.Publish.PublishClient(channel);
+
+            var request = new Toit.Api.Pubsub.PublishRequest
+            {
+                Topic = "cloud:hello-world",
+                PublisherName = "C#",
+                Data = { ByteString.CopyFromUtf8("Hello world") }
+            };
+            await client.PublishAsync(request);
+        }
+    }
+}
